321148f97953862764dae2b986fa3cefa0876d64
Kiryl Dumanski <Kocherizhkin@gmail.com>
2022-12-03 01:00:48 +0300

    Feat: Add post detail view, change the Article model (add slug field)

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from datetime import datetime
 
 from flask import Flask, render_template, request, flash, session, redirect, url_for, abort
 from flask_sqlalchemy import SQLAlchemy
+from slugify import slugify
 from sqlalchemy import desc
+from sqlalchemy.exc import NoResultFound
 
 SECRET_KEY = 'development_key'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///flask-site.db'
@@ -22,11 +24,27 @@ def inject_mainmenu():
     return dict(menu=menu)
 
 
+@app.context_processor
+def utility_processor():
+    return dict(slugify=slugify)
+
+
+@app.template_filter('iso_time')
+def iso_time(s):
+    return s.isoformat(' ', 'seconds')
+
+
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False, unique=True)
     text = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
+    slug = db.Column(db.String(150), unique=True, index=True)
+
+    def __init__(self, *args, **kwargs):
+        if 'slug' not in kwargs:
+            kwargs['slug'] = slugify(kwargs.get('title', ''))
+        super().__init__(*args, **kwargs)
 
     def __repr__(self):
         return f"<Article {self.id}>"
@@ -39,12 +57,23 @@ def index():
     return render_template('index.html', title='Main page', articles=articles)
 
 
+@app.route("/post/<int:post_id>/<string:post_slug>")
+def post_detail(post_id, post_slug):
+    try:
+        article = Article.query.filter_by(id=post_id, slug=post_slug).one()
+    except NoResultFound:
+        return render_template('page404.html', title='Page not found'), 404
+
+    return render_template('post.html', article=article, title=article.title)
+
+
 @app.route("/add-post", methods=["POST", "GET"])
 def add_post():
     if request.method == 'POST':
         try:
             article = Article(title=request.form['title'],
-                              text=request.form['text'])
+                              text=request.form['text'],
+                              slug=slugify(request.form['title']))
             db.session.add(article)
             db.session.flush()
             flash('Post added', category='success')
